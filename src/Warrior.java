9f8c52e81a0db0852b7112b49350e47717d4f3fd
3s3s <eali8242@gmail.com>
2025-12-14 23:19:53 +0200

    add weapon add shape for warrior

--- src/Warrior.java
+++ src/Warrior.java
@@ -1,4 +1,17 @@
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
 public class Warrior extends Fighter {
     public Warrior(double x, double y) {
-        super();
+        super("Warrior", 150, x, y, 3, true, 0, Color.HOTPINK, 150);
+        addWeapon(new Pistol());
+        addWeapon(new Weapon("Sword Slash", 15, 5.0, 700L));
+        addWeapon(new Weapon("Heavy Axe", 30, 3.0, 1500L));
+    }
+    @Override
+    public void createShape() {
+        Shape s = SoldierShapeFactory.createSoldier(getFighterColor(), xPosition, yPosition);
+        this.fighterShape = s;
+        fighterShape.setScaleX(3.5);
+        fighterShape.setScaleY(2.5);
+    }
 }
